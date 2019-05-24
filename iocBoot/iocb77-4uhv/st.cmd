3e0832f2856cc70afee33830f45fa5c7a0366a6e
Michael Davidsaver <mdavidsaver@gmail.com>
2019-05-24 10:12:00 -0700

    add b77 test IOC

---
+++ iocBoot/iocb77-4uhv/st.cmd
@@ -0,0 +1,19 @@
+#!../../bin/linux-x86_64/agilent4uhv
+
+< envPaths
+
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(TOP)/db:.")
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/db")
+
+## Register all support components
+dbLoadDatabase("../../dbd/agilent4uhv.dbd")
+agilent4uhv_registerRecordDeviceDriver(pdbbase)
+
+drvAsynIPPortConfigure("DEV", "192.168.1.10:2115", 0, 0, 0)
+
+#asynSetTraceMask("DEV",0,0x3f)
+#asynSetTraceIOMask("DEV",0,4)
+
+dbLoadRecords("agilent_4UHV.db","P=VTST:,R=4:,PORT=DEV,CH=1")
+
+iocInit()
